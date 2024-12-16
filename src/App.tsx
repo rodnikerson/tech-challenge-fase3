ce4d6fad7551305c8a7422613aaca76a72c1645f
rodnikerson <nikersonrodolpho@gmail.com>
2024-12-16 16:58:02 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="text-3xl font-bold underline text-center">
+        Hello World!
+      </div>
+    </>
+  )
+}
+
+export default App
